f233a598aeb59c54641ea5948d74a0fcab179c21
Vnikolayev <Vnikolayev1@gmail.com>
2020-07-01 01:04:36 +0300

    order_line fix - creates delivery for unchecked items, and another delivery for more than two checked ones.

--- custom_module/models/sale_order_line.py
+++ custom_module/models/sale_order_line.py
@@ -7,17 +7,28 @@ from odoo import fields, models
 _logger = logging.getLogger(__name__)
 
 
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    procurement_express_group_id = fields.Many2one(
+        comodel_name='procurement.group', string='Procurement express Group',
+        copy=False)
+
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     express_shipping = fields.Boolean()
 
+    def _get_procurement_express_group(self):
+        return self.order_id.procurement_express_group_id
+
     def _action_launch_stock_rule(self, previous_product_uom_qty=False):
         """
         Launch procurement group run method with required/custom fields
-        genrated by a sale order line. procurement group will launch
-        '_run_pull', '_run_buy' or '_run_manufacture depending on the
-        sale order line product rule.
+         genrated by a sale order line. procurement group will launch
+          '_run_pull', '_run_buy' or '_run_manufacture' depending on
+           the sale order line product rule.
         """
         precision = self.env['decimal.precision'].precision_get(
             'Product Unit of Measure')
@@ -34,26 +45,62 @@ class SaleOrderLine(models.Model):
             if float_compare(qty, line.product_uom_qty,
                              precision_digits=precision) >= 0:
                 continue
-
-            group_id = self.env['procurement.group'].create(
-                line._prepare_procurement_group_vals())
-            line.order_id.procurement_group_id = group_id
-            values = line._prepare_procurement_values(group_id=group_id)
+            values = False
+            if not line.express_shipping:
+                group_id = line._get_procurement_group()
+                if not group_id:
+                    group_id = self.env['procurement.group'].create(
+                        line._prepare_procurement_group_vals())
+                    line.order_id.procurement_group_id = group_id
+                else:
+                    # In case the procurement group is already created and
+                    # the order was
+                    # cancelled, we need to update certain values of the group.
+                    updated_vals = {}
+                    if group_id.partner_id != \
+                            line.order_id.partner_shipping_id:
+                        updated_vals.update(
+                            {'partner_id': line.order_id.partner_shipping_id.id
+                             })
+                    if group_id.move_type != line.order_id.picking_policy:
+                        updated_vals.update(
+                            {'move_type': line.order_id.picking_policy})
+                    if updated_vals:
+                        group_id.write(updated_vals)
+                values = line._prepare_procurement_values(group_id=group_id)
+            if line.express_shipping and express_lines_amnt >= 2:
+                express_group_id = line._get_procurement_express_group()
+                if not express_group_id:
+                    express_group_id = self.env['procurement.group'].create(
+                        line._prepare_procurement_group_vals())
+                    line.order_id.procurement_express_group_id = \
+                        express_group_id
+                else:
+                    updated_vals = {}
+                    if express_group_id.partner_id != line.order_id.\
+                            partner_shipping_id:
+                        updated_vals.update({
+                            'partner_id':
+                                line.order_id.partner_shipping_id.id})
+                    if express_group_id.move_type != \
+                            line.order_id.picking_policy:
+                        updated_vals.update(
+                            {'move_type': line.order_id.picking_policy})
+                    if updated_vals:
+                        express_group_id.write(updated_vals)
+                values = line._prepare_procurement_values(
+                    group_id=express_group_id)
             product_qty = line.product_uom_qty - qty
-
             line_uom = line.product_uom
-            quant_uom = line.product_id.uom_id
             product_qty, procurement_uom = line_uom._adjust_uom_quantities(
-                product_qty, quant_uom)
-            if express_lines_amnt >= 2:
-                if line.express_shipping:
-                    procurements.append(
-                        self.env['procurement.group'].Procurement(
-                            line.product_id, product_qty, procurement_uom,
-                            line.order_id.partner_shipping_id
-                            .property_stock_customer,
-                            line.name, line.order_id.name,
-                            line.order_id.company_id, values))
+                product_qty, line.product_id.uom_id)
+            if not line.express_shipping or line.express_shipping \
+                    and express_lines_amnt >= 2:
+                procurements.append(self.env['procurement.group'].Procurement(
+                    line.product_id, product_qty, procurement_uom,
+                    line.order_id.partner_shipping_id.property_stock_customer,
+                    line.name, line.order_id.name, line.order_id.company_id,
+                    values))
         if procurements:
             self.env['procurement.group'].run(procurements)
         return True
